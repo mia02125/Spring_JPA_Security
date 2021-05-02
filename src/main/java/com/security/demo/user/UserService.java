8a70175b41657342b39ba930f3e2a7e61a6d7fca
최재민 <mia02125@naver.com>
2021-05-02 16:03:06 +0900

    CREATE 구현 READ 구현

--- src/main/java/com/security/demo/user/UserService.java
+++ src/main/java/com/security/demo/user/UserService.java
@@ -1,5 +1,6 @@
 package com.security.demo.user;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,15 @@ public class UserService {
     }
 
     public void insert(HttpServletResponse response, User user) {
-        User checkUserId = userRepository.findAllByUserId(user.getUserId());
+        // 아이디 체크
+        Boolean checkUserId = userRepository.existsUserByUserId(user.getUserId());
         if(checkUserId == null) {
-            JSUtil.alert_replace(response,"NULL오류 발생","/user");
-        } else if(checkUserId.getUserId().equals(user.getUserId())) {
+            JSUtil.alert_replace(response,"오류 발생 / 오류 사유 : 데이터 NULL","/user");
+        } else if(!checkUserId) {
             BCryptPasswordEncoder PWEncoder = new BCryptPasswordEncoder();
             user.setUserPassword(PWEncoder.encode(user.getUserPassword()));
             userRepository.save(user);
+            JSUtil.alert_replace(response,"저장되었습니다.","/user");
         } else {
             JSUtil.alert_replace(response,"해당 아이디는 존재합니다.","/user/signUp");
         }
